faa3278f81aec05b0c641a7e6e9a9b40cfbceca2
Alisson Matos da Cruz <alissonmcruz@gmail.com>
2018-03-24 19:26:43 -0300

    [Add] Projeto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAnr-s2-1Rg9AlYIC-TadAqurW-5X-y7RE",
+    authDomain: "whoqol-app.firebaseapp.com",
+    databaseURL: "https://whoqol-app.firebaseio.com",
+    projectId: "whoqol-app",
+    storageBucket: "whoqol-app.appspot.com",
+    messagingSenderId: "109335354180"
+  }
 };
