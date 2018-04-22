9671c28156f0a15bd5b815f14c44a742a1a5c41c
Alisson Matos da Cruz <alissonmcruz@gmail.com>
2018-04-22 13:28:53 -0300

    [Add] Uma pah de coisas, incluindo CSV

--- src/app/dados-pesquisa/dados-pesquisa.component.ts
+++ src/app/dados-pesquisa/dados-pesquisa.component.ts
@@ -1,6 +1,11 @@
-import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+
+import { Pesquisa } from '../model/pesquisa.interface';
+import { Angular5Csv } from 'angular5-csv/Angular5-csv';
+
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-dados-pesquisa',
@@ -9,20 +14,37 @@ import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/fires
 })
 export class DadosPesquisaComponent implements OnInit {
 
-  private pesquisaCollection: AngularFirestoreCollection<any>;
-  private pesquisa: Observable<any>;
-  private homens;
+  private pesquisaCollection: AngularFirestoreCollection<Pesquisa>;
+  private pesquisa: Observable<Pesquisa[]>;
+  private tabela;
+  // private ar = [{ 'nome': 'rola', 'cod': 1 }, { 'nome': 'xuxa', 'cod': 2 }, { 'nome': 'toca', 'cod': 3 }];
+  private options = {
+    fieldSeparator: ';',
+    quoteStrings: '"',
+    decimalseparator: ',',
+    showLabels: true,
+    headers: ["Nome","Idade","Sexo","Estado Civil","Escolaridade","Profissao","Email","Respostas",
+    "Ajuda","Tempo","Dom Fisico","Dom Psicologico","Dom Rel Sociais","Dom Meio Ambiente","Score"]
+  };
 
   constructor(private afs: AngularFirestore) {
+    // let cuzin = new Angular5Csv(this.ar, 'Dados', this.options);
+    // console.log(cuzin);
 
   }
 
   ngOnInit() {
     this.pesquisaCollection = this.afs.collection('pesquisa');
     this.pesquisa = this.pesquisaCollection.valueChanges();
-    // this.pesquisa.forEach(el => {
-    //   this.homens = el.length;
-    // });
+    this.pesquisa.subscribe(data => {
+      this.tabela = data;
+      console.log(this.tabela);
+    });
+  }
+
+  toCSV() {
+    console.log("Exportando");
+    new Angular5Csv(this.tabela, 'WhoqolAppDados', this.options)
   }
 
 }
---
+++ src/app/model/pesquisa.interface.ts
@@ -0,0 +1,17 @@
+export interface Pesquisa {
+  _01_nome: string;
+  _02_idade: number;
+  _03_sexo: string;
+  _04_estadoCivil: string;
+  _05_escolaridade: string;
+  _06_profissao: string;
+  _07_email: string;
+  _08_respostas: Array<number>;
+  _09_ajuda: string;
+  _10_tempo: string;
+  _11_domFisico: number;
+  _12_domPsico: number;
+  _13_domRelSoc: number;
+  _14_domMeioAmb: number;
+  _15_score: number;
+}
