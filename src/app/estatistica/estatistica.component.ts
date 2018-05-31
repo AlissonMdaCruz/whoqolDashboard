de2d809995d3bed847319cab7e4ccc944a79c4c5
Alisson Matos da Cruz <alissonmcruz@gmail.com>
2018-05-31 18:42:34 -0300

    [Add] desvio padrao e variancia

--- src/app/estatistica/estatistica.component.ts
+++ src/app/estatistica/estatistica.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Pesquisa } from '../model/pesquisa.interface';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument
+} from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import { StatisticsService } from '../providers/statistics.service';
 
@@ -10,32 +14,42 @@ import { StatisticsService } from '../providers/statistics.service';
   styleUrls: ['./estatistica.component.css']
 })
 export class EstatisticaComponent implements OnInit {
-
   public pesquisaCollection: AngularFirestoreCollection<Pesquisa>;
   public pesquisa: Observable<Pesquisa[]>;
   public mediaGeral: number;
   public medianaGeral: number;
+  public desvioPadrao: string;
+  public variancia: any;
   public total: number;
   public teste = [54, 34, 1, 24, 3, 89, 5, 7, 34, 1];
 
-  constructor(public afs: AngularFirestore, public est: StatisticsService) { }
+  constructor(public afs: AngularFirestore, public est: StatisticsService) {}
 
   ngOnInit() {
     this.pesquisaCollection = this.afs.collection('pesquisa');
     this.pesquisa = this.pesquisaCollection.valueChanges();
     this.pesquisa.forEach(element => {
+      // recupera soh os scores
       this.getScore(element).then(resp => {
         this.total = resp.length;
-        this.mediaGeral = this.est.media(resp);
+        // pega a media geral
+        this.est.media(resp).then(med => {
+          this.mediaGeral = med.toFixed(2);
+          // calcula a variancia
+          this.est.variancia(resp, med).then(a => {
+            this.variancia = a;
+            this.desvioPadrao = this.est.desvioPadrao(a).toFixed(4);
+          });
+        });
         this.medianaGeral = this.est.mediana(resp);
-        console.log(resp);
+        // console.log(resp);
       });
     });
   }
 
   // Devolve um array soh com os scores
   getScore(arr: Array<any>): Promise<any> {
-    return new Promise((resolve) => {
+    return new Promise(resolve => {
       const newArr = [];
       arr.forEach(el => {
         newArr.push(el._15_score);
@@ -43,5 +57,4 @@ export class EstatisticaComponent implements OnInit {
       resolve(newArr.sort((a, b) => a - b));
     });
   }
-
 }
--- src/app/providers/statistics.service.ts
+++ src/app/providers/statistics.service.ts
@@ -2,24 +2,48 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class StatisticsService {
+  constructor() {}
 
-  constructor() { }
-
-  media(arr: Array<any>) {
-    let aux = 0;
-    const tam = arr.length;
-    arr.forEach(el => {
-      aux += el;
+  media(arr: Array<any>): Promise<any> {
+    return new Promise(resolve => {
+      let aux = 0;
+      const tam = arr.length;
+      arr.forEach(el => {
+        aux += el;
+      });
+      resolve(aux / tam);
     });
-    return aux / tam;
   }
 
   mediana(arr: Array<any>) {
     const i = Math.floor(arr.length / 2);
-    if (arr.length % 2 === 0 ) {
+    if (arr.length % 2 === 0) {
       return (arr[i] + arr[i - 1]) / 2;
-    }else {
+    } else {
       return arr[i];
     }
+  }
+
+  /**
+   * O desvio padrão indica o quanto um conjunto de dados é uniforme.
+   * Quanto mais próximo de 0 for o desvio padrão, mais homogêneo são os dados.
+   * É a raiz quadrada da variância.
+   */
+  desvioPadrao(desvio: number) {
+    return Math.sqrt(desvio);
+  }
+
+  /**
+   * O desvio padrão indica o quanto um conjunto de dados é uniforme.
+   * Quanto mais próximo de 0 for o desvio padrão, mais homogêneo são os dados.
+   */
+  async variancia(arr: Array<any>, med: number) {
+    let aux = 0,
+      result;
+    await arr.forEach(el => {
+      aux += Math.pow(el - med, 2);
+    });
+    result = Math.sqrt(aux / arr.length);
+    return result.toFixed(4);
   }
 }
